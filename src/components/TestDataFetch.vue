5b3dc974ea8292fa3458d5d33a9a77ed54bb1e8b
Christopher Knight <chris@knightideas.co.uk>
2025-03-17 12:25:59 +0000

    Create a simpler test for the update

--- src/components/TestDataFetch.vue
+++ src/components/TestDataFetch.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h2>JSONBin Data Test</h2>
-    <pre>{{ testData }}</pre>
-  </div>
+    <div>
+        <h2>JSONBin Data Test</h2>
+        <pre>{{ testData }}</pre>
+    </div>
 </template>
 
 <script>
@@ -10,6 +10,36 @@ export default {
     data() {
         return {
             testData: null
+        }
+    },
+    method: {
+        addData() {
+            // Add data to the JSONBIN.io database with the method of PUT
+            fetch(`https://api.jsonbin.io/v3/b/${import.meta.env.VITE_JSONBIN_ID}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Master-Key': import.meta.env.VITE_JSONBIN_X_MASTER_KEY,
+                    "X-Access-Key": import.meta.env.VITE_JSONBIN_X_ACCESS_KEY
+                },
+                body: JSON.stringify({
+                    id: Date.now().toString(), // ✅ unique ID
+                    event: 'Test Event',
+                    description: 'This is a test prediction 2.',
+                    date: '2050-01-01',
+                    disaster_type: 'test',
+                    author: 'Test Author',
+                    source: 'https://example.com',
+                    status: 'impending',
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Data added successfully', data)
+                })
+                .catch(error => {
+                    console.error('There was an error!', error)
+                })
         }
     },
     created() {
@@ -20,22 +50,22 @@ export default {
                 "X-Access-Key": import.meta.env.VITE_JSONBIN_X_ACCESS_KEY
             }
         })
-        .then(response => response.json())
-        .then(data => {
-            this.testData = data.record
-            console.log('Data fetched successfully', data)
-            // Output the environment variables
-            console.log(import.meta.env.VITE_JSONBIN_ID)
-            console.log('X Master Key', import.meta.env.VITE_JSONBIN_X_MASTER_KEY)
-            console.log('X Access Key', import.meta.env.VITE_JSONBIN_X_ACCESS_KEY)
-        })
-        .catch(error => {
-            console.error('There was an error!', error)
-        })
+            .then(response => response.json())
+            .then(data => {
+                this.testData = data.record
+                console.log('Data fetched successfully', data)
+                // Output the environment variables
+                console.log(import.meta.env.VITE_JSONBIN_ID)
+                console.log('X Master Key', import.meta.env.VITE_JSONBIN_X_MASTER_KEY)
+                console.log('X Access Key', import.meta.env.VITE_JSONBIN_X_ACCESS_KEY)
+            })
+            .catch(error => {
+                console.error('There was an error!', error)
+            })
+            // Add data to the JSONBIN.io database
+            this.addData()  
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
